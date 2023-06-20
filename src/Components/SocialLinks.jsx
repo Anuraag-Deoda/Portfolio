5181728754dbc9331a56cd615aa498c58f2d6676
Your anurag <anuraagdeoda810@gmail.com>
2023-06-20 10:30:47 +0530

    first commit

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -5,13 +5,13 @@ import { icons } from "../Constants/constants";
 const SocialLinks = () => {
   return (
     <div className="w-full h-[100px] bg-grayscale-950 flex justify-center items-center gap-3 p-6">
-      <a href="https://github.com/anu" target="_blank">
+      <a href="https://github.com/Anuraag-Deoda" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faGithub}
         />
       </a>
-      <a href="mailto:Anuraagdeoda@gmail.com" target="_blank">
+      <a href="mailto:anuraagdeoda@gmail.com" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
           icon={icons.faEnvelope}
